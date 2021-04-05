9c8f637ae659421cf0f92f90c6196cd44e479aa1
Mostafa Nassr <mostafa.nassr88@gmail.com>
2021-04-05 13:37:04 -0400

    trying to fix postgresql deployment error

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql:///car-traders')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('HEROKU_DATABASE_URL','postgresql:///car-traders')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "secret")
 
@@ -345,6 +345,3 @@ def delete_trade(id):
 
     flash("Trade successfully deleted", "info")
     return redirect(f'/{trade.user.id}')
-
-if __name__ == '__main__':
-    db.app.run()
